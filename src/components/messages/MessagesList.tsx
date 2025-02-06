fc8facceffcda9fb563b713a115f7c8112feb40a
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2025-02-07 05:47:25 +0600

    Added loaders

--- src/components/messages/MessagesList.tsx
+++ src/components/messages/MessagesList.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { UserRound } from 'lucide-react';
+import { Loader, LoaderCircle, UserRound } from 'lucide-react';
 import Message from '@/components/messages/Message.tsx';
 import useGetMessages from '@/hooks/useGetMessages.tsx';
 
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const MessagesList: FC<Props> = ({id}) => {
-  const {sortedMessages} = useGetMessages();
+  const {sortedMessages, loading} = useGetMessages();
   return (
     <div className="w-full flex flex-col">
       <div className="h-[60px] px-[16px] bg-light-gray flex items-center">
@@ -16,9 +16,16 @@ const MessagesList: FC<Props> = ({id}) => {
         <p className="ml-3">{id}</p>
       </div>
       <div className="flex-1 overflow-y-auto overflow-x-hidden scroll-smooth px-5 py-4 flex flex-col-reverse">
-        {sortedMessages.map((msg) => (
-          <Message msg={msg} incoming={msg.type === 'incoming'} key={msg.idMessage}/>
-        ))}
+        {
+          loading ?
+            <div className="h-full flex items-center justify-center">
+              <Loader size={60} className="animate-spin"/>
+            </div>
+            :
+            sortedMessages.map((msg) => (
+              <Message msg={msg} incoming={msg.type === 'incoming'} key={msg.idMessage}/>
+            ))
+        }
       </div>
     </div>
   );
--- src/hooks/useGetMessages.tsx
+++ src/hooks/useGetMessages.tsx
@@ -5,21 +5,28 @@ import { useParams } from 'react-router-dom';
 
 const UseGetMessages = () => {
   const {id} = useParams();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const {idInstance, apiTokenInstance} = useAppContext();
   const [messages, setMessages] = useState([]);
   const sortedMessages = messages.sort((a, b) => b.timestamp - a.timestamp);
 
   useEffect(() => {
+    setLoading(true);
     setMessages([]);
     axiosApi.post(`/waInstance${idInstance}/getChatHistory/${apiTokenInstance}`, {
       chatId: id,
       count: 30
     }).then((response) => {
       setMessages(response.data);
+    }).catch((e) => {
+      setError(e);
+    }).finally(() => {
+      setLoading(false);
     })
   }, [id]);
 
-  return {sortedMessages}
+  return {sortedMessages, loading}
 };
 
 export default UseGetMessages;
